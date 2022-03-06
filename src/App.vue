21584751abc023fcee37a0dd79ae5866bf639c1b
Judah Perez <judah@cookiedragon.games>
2022-03-05 20:14:36 -0800

    Treasure Dungeon Randomizer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,130 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Final Fantasy XIV Treasure Dungeon Door Randomizer!</h1>
+
+<template v-if="!calculating">
+  <p>
+    Pick a door, any door! Run those treasure dungeons with the power of randomness!
+  </p>
+  <h2>Left or Right?</h2>
+  <div>
+    <label><input type="checkbox" v-model="alwaysLeft"> Josh Algorithm</label>
+  </div>
+  <br/>
+<div>
+  <button @click="calc()" > Calculate </button>
+</div>
+</template>
+<template v-else>
+<transition >
+  <div>
+    Calculating base decision...
+  </div>
+</transition>
+
+<transition >
+  <div v-if="calcStep>=1">
+    Grabbing background radiation...
+  </div>
+</transition>
+
+<transition >
+  <div v-if="calcStep>=2">
+    Checking local tide and wave height...
+  </div>
+</transition>
+  
+<transition >
+  <div v-if="calcStep>=3">
+    Finding prime numbers...
+  </div>
+</transition>
+  
+<transition>
+  <div v-if="calcStep>=4 && alwaysLeft">
+    Applying Josh Algorithm...
+  </div>
+</transition>
+<br/>
+<transition>
+  <div v-if="calcDone">
+    <h2>And the result is!</h2>
+    <h3 v-if="left">
+      Left
+    </h3>
+    <h3 v-else>
+      Right
+    </h3>
+    <br/>
+    <div>
+      <button @click="reroll()" > Reroll </button>
+    </div>
+    <div>
+      <button @click="reset()" > Settings </button>
+    </div>
+  </div>
+
+</transition>
+</template>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      alwaysLeft: true,
+      calculating: false,
+      calcStep: 0,
+      calcDone: false,
+      left: false,
+    }
+  },
+  methods: {
+    calc: function() {
+      this.calculating = true;
+      
+      this.CalcTimer();
+
+    },
+    CalcTimer: function() {
+      var self = this;
+      setTimeout(function(){ 
+        var maxStep = 4;
+        if(self.alwaysLeft) {
+          maxStep = 5;
+        }
+        self.calcStep++;
+
+        self.left = self.getRandomInt(2);
+        if(self.calcStep >= 4 && self.alwaysLeft) {
+          self.left = true;
+        }
+
+        if(self.calcStep >= maxStep) {
+          self.calcDone = true;
+        } 
+
+        if(self.calcDone == false) {
+          self.CalcTimer();
+        }
+        
+        }, 760 + (74 * self.calcStep));
+    },
+    reset: function() {
+      this.calculating = false;
+      this.calcStep = 0;
+      this.calcDone = false;
+    },
+    reroll: function() {
+      this.reset();
+      this.calc();
+    },
+    getRandomInt: function (max) {
+      return Math.floor(Math.random() * max);
+  }
   }
 }
 </script>
@@ -23,4 +138,16 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+
 </style>
